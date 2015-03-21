21cfc32c1c73f6c362c64cf6d3e395a14702cdc2
Todd Chisholm <tchisholm@urthecast.com>
2015-03-21 10:42:25 -0700

    Added water menus and first cut flow display

--- controller/controller.ino
+++ controller/controller.ino
@@ -15,7 +15,7 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 Adafruit_FT6206 ctp = Adafruit_FT6206();
 unsigned int pump_cycle_millis = 5000;
 unsigned int sol_update_millis = 100;
-unsigned int flow_update_millis = 2000;
+unsigned int flow_update_millis = 1000;
 
 unsigned int pump_percent = 0;
 boolean pump_on = false;
@@ -24,6 +24,10 @@ unsigned long sol_update_time = 0;
 unsigned long flow_update_time = 0;
 
 volatile unsigned long int flow_count = 0;
+unsigned long int prev_flow_count = 0;
+
+// Factor to convert flow count to gallons
+float flow_factor = .001;
 
 int pump_pin = 7;
 int solenoid_pin = 4;
@@ -42,6 +46,11 @@ struct float_data {
 float_data temp1 = {0+100, 0};
 float_data temp2 = {0+100, 0};
 float_data heater1_percent = {0+100,0};
+float_data heater2_percent = {0+100,0};
+
+enum MenuPages { TOP, BOIL, PUMPACROSS, MASH, PUMPOUT, ADDWATER};
+
+MenuPages menu = TOP;
 
 byte strt_mark = 255;
 
@@ -67,6 +76,17 @@ void send_heater1_percent(float percent) {
   Serial1.write(heater1_percent.dec_part);
 }
 
+void send_heater2_percent(float percent) {
+  // Also sets the current heater2_percent
+  heater2_percent = float_to_float_data(percent);
+  Serial1.write(strt_mark);
+  // Temperature 2 percentage
+  byte heater2_percent_mark = 4;
+  Serial1.write(heater2_percent_mark);
+  Serial1.write(heater2_percent.int_part);
+  Serial1.write(heater2_percent.dec_part);
+}
+
 void change_pump(boolean oo) {
   if (oo) {
     digitalWrite(pump_pin, HIGH);  
@@ -80,50 +100,92 @@ void service_flow_pin() {
   flow_count += 1;
 }
 
+// Each of the display routines is responsible for checking the menu
 void display_temp1() {
-  tft.fillRect(60,200,100,40,ILI9341_BLACK);
-  tft.setCursor(60, 200);
-  tft.println(String(int(temp1.int_part)-100)+"."+String(temp1.dec_part)+"C");
+  if (menu == BOIL) {
+    tft.fillRect(60,200,100,40,ILI9341_BLACK);
+    tft.setCursor(60, 200);
+    tft.println(String(int(temp1.int_part)-100)+"."+String(temp1.dec_part)+"C");
+  }
 }
 void display_temp2() {
-  //tft.fillRect(60,200,100,40,ILI9341_BLACK);
-  //tft.setCursor(60, 200);
-  //tft.println(String(int(temp1.int_part)-100)+"."+String(temp1.dec_part)+"C");
+  if (menu == PUMPACROSS || menu == MASH) {
+    tft.fillRect(60,200,100,40,ILI9341_BLACK);
+    tft.setCursor(60, 200);
+    tft.println(String(int(temp2.int_part)-100)+"."+String(temp2.dec_part)+"C");
+  }
 }
 
 void display_heater1() {
-  tft.fillRect(25,140,100,40,ILI9341_BLACK);
-  tft.setCursor(25, 140);
-  tft.println(String(int(heater1_percent.int_part)-100)+"."+String(heater1_percent.dec_part));
+  if (menu == BOIL) {
+    tft.fillRect(25,140,120,40,ILI9341_BLACK);
+    tft.setCursor(25, 140);
+    tft.println(String(int(heater1_percent.int_part)-100)+"."+String(heater1_percent.dec_part));
+  }
+}
+void display_heater2() {
+  if (menu == PUMPACROSS || menu == MASH) {
+    tft.fillRect(25,140,120,40,ILI9341_BLACK);
+    tft.setCursor(25, 140);
+    tft.println(String(int(heater2_percent.int_part)-100)+"."+String(heater2_percent.dec_part));
+  }
+}
+void display_water_count() {
+  if ( menu==ADDWATER && prev_flow_count != flow_count) {
+    tft.fillRect(110,150,125,40,ILI9341_BLACK);
+    tft.setCursor(110, 150);
+    float gallons = float(flow_count)*flow_factor;
+    int gallons_int_part = int(gallons);
+    int gallons_dec_part1 = int((gallons-float(gallons_int_part))*10.);
+    int gallons_dec_part2 = int((gallons-float(gallons_dec_part1)/10.-float(gallons_int_part))*100.);
+    prev_flow_count = flow_count;
+    tft.println(String(gallons_int_part)+"."+String(gallons_dec_part1)+String(gallons_dec_part2)+"G");
+  }
 }
 
-void setup() {
-  Serial1.begin(9600);
-  Serial.begin(9600);
-
-  pinMode(pump_pin, OUTPUT);
-  pinMode(solenoid_pin, OUTPUT);  
-  pinMode(wtr_rqst_pin, INPUT);
-  digitalWrite(wtr_rqst_pin, HIGH);
-  
-  pinMode(flow_pin, INPUT);
-  digitalWrite(solenoid_pin, LOW);
-  attachInterrupt(0, service_flow_pin, RISING);
-  tft.begin();
-  ctp.begin(40);
-  // read diagnostics (optional but can help debug problems)
-  //uint8_t x = tft.readcommand8(ILI9341_RDMODE);
-  //x = tft.readcommand8(ILI9341_RDMADCTL);
-  //x = tft.readcommand8(ILI9341_RDPIXFMT);
-  //x = tft.readcommand8(ILI9341_RDIMGFMT);
-  //x = tft.readcommand8(ILI9341_RDSELFDIAG);
-
+void prepare_top_menu() {
   tft.fillScreen(ILI9341_BLACK);
-  //unsigned long start = micros();
-  tft.setRotation(3);
+  tft.setTextSize(4);
+  tft.setCursor(0, 0);
+  tft.println("BOIL");  
+  tft.setCursor(0, 45);
+  tft.println("PUMP TO MASH");  
+  tft.setCursor(0, 90);
+  tft.println("MASH");  
+  tft.setCursor(0, 135);
+  tft.println("PUMP OUT");  
+  tft.setCursor(0, 180);
+  tft.println("ADD WATER");  
+}
+
+void prepare_boil_menu() {
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(110, 0);
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
+  tft.println("MENU");
   tft.setCursor(0, 0);
   tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
-  tft.println("HEATER");
+  tft.println("HEAT");
+  tft.setTextColor(ILI9341_WHITE);
+  tft.drawRect(0,40,20,200,ILI9341_RED);
+  tft.setCursor(25, 140);
+  tft.println("0%");
+  tft.setCursor(35, 40);
+  tft.println("OFF");  
+  tft.setCursor(42, 80);
+  tft.println("ON");  
+  
+  display_temp1();
+}
+
+void prepare_pumpacross_menu() {
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(110, 0);
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
+  tft.println("MENU");
+  tft.setCursor(0, 0);
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
+  tft.println("HEAT");
   tft.setTextColor(ILI9341_WHITE);
   tft.drawRect(0,40,20,200,ILI9341_RED);
   tft.setCursor(25, 140);
@@ -150,8 +212,173 @@ void setup() {
   tft.setCursor(220, 200);
   tft.println("100%");
 
-  display_temp1();
   display_temp2();
+}
+
+void prepare_mash_menu() {
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(110, 0);
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
+  tft.println("MENU");
+  tft.setCursor(0, 0);
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
+  tft.println("HEAT");
+  tft.setTextColor(ILI9341_WHITE);
+  tft.drawRect(0,40,20,200,ILI9341_RED);
+  tft.setCursor(25, 140);
+  tft.println("0%");
+  tft.setCursor(35, 40);
+  tft.println("OFF");  
+  tft.setCursor(42, 80);
+  tft.println("ON");  
+  
+  display_temp2();
+}
+
+void prepare_pumpout_menu() {
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(110, 0);
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
+  tft.println("MENU");
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
+  tft.setCursor(220, 0);
+  tft.setTextColor(ILI9341_BLUE);
+  tft.println("PUMP");
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setCursor(220, 40);
+  tft.setTextColor(ILI9341_GREEN);
+  tft.println("0%");
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setCursor(220, 80);
+  tft.println("25%");
+  tft.setCursor(220, 120);
+  tft.println("50%");
+  tft.setCursor(220, 160);
+  tft.println("75%");  
+  tft.setCursor(220, 200);
+  tft.println("100%");
+}
+
+void prepare_addwater_menu() {
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(110, 0);
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
+  tft.println("MENU");
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(4);
+  tft.setCursor(0, 0);
+  tft.setTextColor(ILI9341_BLUE);
+  tft.println("RESET");
+  display_water_count();
+}
+
+void clear_all() {
+  // Pump off
+  pump_percent = 0;
+  pump_on = false;  
+  change_pump(pump_on);
+
+  // Heaters off
+  send_heater1_percent(0.0);
+  send_heater2_percent(0.0);
+
+}
+
+void check_pump_touch(int xx, int yy) {
+  // Pump change
+  if (xx>=220 && yy>=40) {
+    int pos = (yy-40)/40;
+    tft.fillRect(220,40,100,200,ILI9341_BLACK);
+    pump_percent = pos*25;
+    for (int ii=0; ii<5; ii++) {
+      tft.setCursor(220, ii*40+40);
+      if (ii==pos) {
+	tft.setTextColor(ILI9341_GREEN);
+      }
+      else {
+	tft.setTextColor(ILI9341_WHITE);
+      }
+      tft.println(String(ii*25)+"%");
+    }
+    tft.setTextColor(ILI9341_WHITE);
+    if (pump_percent != 100) {
+      pump_on = false;  
+      pump_transition_time = millis()+int(float(pump_cycle_millis)*(1.-float(pump_percent)/100.));
+      change_pump(pump_on);
+    }
+    else {
+      // pump 100%
+      pump_on = true;  
+      change_pump(pump_on);
+    }
+  }
+}
+
+void check_heater_touch(int xx, int yy, int heater_num, int heater_range) 
+{
+  // Heater slider
+  if (xx <= 30 && yy >= 40) {
+    tft.fillRect(25,140,100,40,ILI9341_BLACK);
+    tft.setCursor(25, 140);
+    float heater_percent_f = float(100-((yy-40)/2))/100.*heater_range;
+    if (heater_num == 1) {
+      send_heater1_percent(heater_percent_f);
+      display_heater1();
+    }
+    else {
+      send_heater2_percent(heater_percent_f);
+      display_heater2();
+    }
+  }
+  // Heater On/Off
+  else if (xx >= 35 && xx <=135 && yy >= 40 && yy <=120) {
+    tft.fillRect(25,140,100,40,ILI9341_BLACK);
+    tft.fillRect(25,140,80,40,ILI9341_BLACK);
+    tft.setCursor(25, 140);
+    if (yy >= 80) {
+      if (heater_num == 1) {
+	send_heater1_percent(float(heater_range));
+	display_heater1();
+      }
+      else {
+	send_heater2_percent(float(heater_range));
+	display_heater2();
+      }
+    }
+    else {
+      if (heater_num == 1) {
+	send_heater1_percent(0.0);
+	display_heater1();
+      }
+      else {
+	send_heater2_percent(0.0);
+	display_heater2();
+      }
+    }
+  }
+}
+
+void check_menu_touch(int xx, int yy) {
+  if (xx>=110 && xx <= 210 && yy<=40) {
+    menu = TOP;
+    clear_all();
+    prepare_top_menu();
+  }
+}
+
+void setup() {
+  Serial1.begin(9600);
+  Serial.begin(9600);
+
+  pinMode(pump_pin, OUTPUT);
+  pinMode(solenoid_pin, OUTPUT);  
+  pinMode(wtr_rqst_pin, INPUT);
+  digitalWrite(wtr_rqst_pin, HIGH);
+  
+  pinMode(flow_pin, INPUT);
+  digitalWrite(solenoid_pin, LOW);
+  attachInterrupt(0, service_flow_pin, RISING);
+  tft.begin();
+  ctp.begin(40);
 
   sol_update_time = millis()+sol_update_millis;
   flow_update_time = millis()+flow_update_millis;
@@ -159,13 +386,17 @@ void setup() {
   // Clear the serial buffer
   char message[61];
   Serial1.readBytes(message, 61);
+  tft.fillScreen(ILI9341_BLACK);
 
+  tft.setRotation(3);
+  clear_all();
+  prepare_top_menu();
   return;
 }
 
 void loop(void) {
-  if (ctp.touched()) {
 
+  if (ctp.touched()) {
     // Retrieve a point  
     TS_Point p = ctp.getPoint();
   
@@ -175,55 +406,68 @@ void loop(void) {
     int xx = p.y;
     int yy = p.x;
 
-    // Heater slider
-    if (xx <= 30 && yy >= 40) {
-      tft.fillRect(25,140,100,40,ILI9341_BLACK);
-      tft.setCursor(25, 140);
-      float heater_percent_f = 100-((yy-40)/2);
-      send_heater1_percent(heater_percent_f);
-      display_heater1();
-    }
-    // Heater On/Off
-    else if (xx >= 35 && xx <=135 && yy >= 40 && yy <=120) {
-      tft.fillRect(25,140,100,40,ILI9341_BLACK);
-      tft.fillRect(25,140,80,40,ILI9341_BLACK);
-      tft.setCursor(25, 140);
-      if (yy >= 80) {
-        send_heater1_percent(100.0);
+    switch (menu)
+      {
+      case TOP:
+	if ( xx <=160 && yy >= 0 && yy <45) {
+	  // Boil
+	  clear_all();
+	  menu = BOIL;
+	  prepare_boil_menu();
+	}
+	else if ( xx <=160 && yy >= 45 && yy <90) {
+	  // Pump across
+	  clear_all();
+	  menu = PUMPACROSS;
+	  prepare_pumpacross_menu();
+	}
+	else if ( xx <=160 && yy >= 90 && yy <135) {
+	  // Mash
+	  clear_all();
+	  menu = MASH;
+	  prepare_mash_menu();
+	}
+	else if ( xx <=160 && yy >= 135 && yy <180) {
+	  // Pump out
+	  clear_all();
+	  menu = PUMPOUT;
+	  prepare_pumpout_menu();
+	}
+	else if ( xx <=160 && yy >= 180 && yy <225) {
+	  // Add water
+	  clear_all();
+	  menu = ADDWATER;
+	  prepare_addwater_menu();
+	}
+	break;
+      case BOIL:
+	check_heater_touch(xx, yy, 1, 100);
+	check_menu_touch(xx,yy);
+	break;
+      case PUMPACROSS:
+	check_heater_touch(xx, yy, 2, 100);
+	check_pump_touch(xx, yy);
+	check_menu_touch(xx,yy);
+	break;
+      case MASH:
+	check_heater_touch(xx, yy, 2, 25);
+	check_menu_touch(xx,yy);
+	break;
+      case PUMPOUT:
+	check_pump_touch(xx, yy);
+	check_menu_touch(xx,yy);
+	break;
+      case ADDWATER:
+	check_menu_touch(xx,yy);
+	if (xx<=125 && yy<=40) {
+	  flow_count = 0;
+	  display_water_count();
+	}
+	break;
       }
-      else {
-        send_heater1_percent(0.0);
-      }
-      display_heater1();
-    }
-    // Pump change
-    else if (xx>=220 && yy>=40) {
-      int pos = (yy-40)/40;
-      tft.fillRect(220,40,100,200,ILI9341_BLACK);
-      pump_percent = pos*25;
-      for (int ii=0; ii<5; ii++) {
-        tft.setCursor(220, ii*40+40);
-        if (ii==pos) {
-          tft.setTextColor(ILI9341_GREEN);
-        }
-        else {
-          tft.setTextColor(ILI9341_WHITE);
-        }
-        tft.println(String(ii*25)+"%");
-      }
-      tft.setTextColor(ILI9341_WHITE);
-      if (pump_percent != 100) {
-        pump_on = false;  
-        pump_transition_time = millis()+int(float(pump_cycle_millis)*(1.-float(pump_percent)/100.));
-        change_pump(pump_on);
-      }
-      else {
-        // pump 100%
-        pump_on = true;  
-        change_pump(pump_on);
-      }
-    }
   } 
+
+  // Check for pump transition
   if (pump_percent != 0 && pump_percent != 100) {
     if (millis() > pump_transition_time) {
       if (pump_on) {
@@ -239,13 +483,13 @@ void loop(void) {
     }
   }
 
+  // Flow display update
   if (millis() > flow_update_time) {
-    //tft.fillRect(60,160,140,40,ILI9341_BLACK);
-    //tft.setCursor(60, 160);
-    //tft.println(flow_count/10);
+    display_water_count();
     flow_update_time = millis()+flow_update_millis;  
   }
 
+  // Water solenoid update
   if (millis() > sol_update_time) {
     if (digitalRead(wtr_rqst_pin) == LOW) {
       digitalWrite(solenoid_pin, HIGH);
